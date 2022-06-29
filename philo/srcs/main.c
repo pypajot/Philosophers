f0d6dc19da0b140c47a2673e99765fb82f4ba3ec
Pierre-yves PAJOT <ppajot@e2r12p16.42.fr>
2022-06-29 16:46:48 +0200

    cleaning

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ppajot <ppajot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 20:24:04 by ppajot            #+#    #+#             */
-/*   Updated: 2022/06/27 20:11:28 by ppajot           ###   ########.fr       */
+/*   Updated: 2022/06/29 16:45:40 by ppajot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,69 +22,112 @@ void	display(long t_zero, int id, pthread_mutex_t *displaymutex, char *str)
 	pthread_mutex_unlock(displaymutex);
 }
 
+void	takes_put_fork(t_data *data, int id, int forknbr, int takes_put)
+{
+	int fork;
+	
+	if (forknbr == 2)
+		fork = ((id / 2) * 2) % data->philnbr;
+	else
+		fork = (((id - 1) / 2) * 2 + 1) % data->philnbr;
+	pthread_mutex_lock(&data->forkmutex[fork]);
+	data->forktab[fork] = takes_put;
+	pthread_mutex_unlock(&data->forkmutex[fork]);
+	if (takes_put == 0)
+		display(data->t_zero, id, &data->display, "has taken a fork");
+}
+
+int	check_fork(t_data *data, int id, int forknbr)
+{
+	int	res;
+	int	fork;
+	
+	if (forknbr == 2)
+		fork = ((id / 2) * 2) % data->philnbr;
+	else
+		fork = (((id - 1) / 2) * 2 + 1) % data->philnbr;
+	pthread_mutex_lock(&data->forkmutex[fork]);
+	if (data->forktab[fork])
+		res = 1;
+	else
+		res = 0;
+	pthread_mutex_unlock(&data->forkmutex[fork]);
+	return (res);
+}
+
+int	check_end(t_data *data)
+{
+	int	res;
+	
+	pthread_mutex_lock(&data->endmutex);
+	if (data->end)
+		res = 1;
+	else
+		res = 0;
+	pthread_mutex_unlock(&data->endmutex);
+	return (res);
+}
+
+void	think(t_data *data, int id, int *eat_sleep_think)
+{
+	display(data->t_zero, id, &data->display, "is thinking");
+	*eat_sleep_think = 0;
+}
+
+void	ft_sleep(t_data *data, int id, int *eat_sleep_think)
+{
+	display(data->t_zero, id, &data->display, "is sleeping");
+	usleep(data->timetosleep * 1000);
+	*eat_sleep_think = 3;
+}
+
+void	eat(t_data *data, int id, int *eat_sleep_think)
+{
+	struct timeval	tv;
+	
+	takes_put_fork(data, id, 2, 0);	
+	pthread_mutex_lock(&data->lasteatmutex);
+	gettimeofday(&tv, 0);
+	data->lasteat[id - 1] = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	pthread_mutex_unlock(&data->lasteatmutex);
+	display(data->t_zero, id, &data->display, "is eating");
+	pthread_mutex_lock(&data->meals);
+	data->mealcount[id - 1]++;
+	pthread_mutex_unlock(&data->meals);
+	usleep(data->timetoeat * 1000);
+	takes_put_fork(data, id, 1, 1);
+	takes_put_fork(data, id, 2, 1);
+	*eat_sleep_think = 2;
+}
+
 void	*philo(void *arg)
 {
 	t_data *data = (t_data *)arg;
-	struct timeval tv;
-	int		eat_sleep_think = 0;
+	int		eat_sleep_think;
 	int		id;
 	
+	eat_sleep_think = 0;
 	pthread_mutex_lock(&data->idmutex);	
 	id = ++data->philid;
 	pthread_mutex_unlock(&data->idmutex);
-	pthread_mutex_lock(&data->endmutex);
-	while (data->end)
+	while (check_end(data))
 	{
-		pthread_mutex_unlock(&data->endmutex);
 		if (eat_sleep_think == 3)
-		{
-			display(data->t_zero, id, &data->display, "is thinking");
-			eat_sleep_think = 0;
-		}
+			think(data, id, &eat_sleep_think);
 		if (eat_sleep_think == 2)
+			ft_sleep(data, id, &eat_sleep_think);
+		if (eat_sleep_think == 1 && check_fork(data, id, 2))
+			eat(data, id, &eat_sleep_think);		
+		if (eat_sleep_think == 0 && check_fork(data, id, 1))
 		{
-			display(data->t_zero, id, &data->display, "is sleeping");
-			usleep(data->timetosleep * 1000);
-			eat_sleep_think = 3;
-		}
-		pthread_mutex_lock(&data->forkmutex[((id / 2) * 2) % data->philnbr]);
-		if (data->forktab[((id / 2) * 2) % data->philnbr] && eat_sleep_think == 1)
-		{			
-			data->forktab[((id / 2) * 2) % data->philnbr] = 0;
-			pthread_mutex_unlock(&data->forkmutex[((id / 2) * 2) % data->philnbr]);
-			display(data->t_zero, id, &data->display, "has taken a fork");
-			gettimeofday(&tv, 0);
-			pthread_mutex_lock(&data->lasteatmutex);
-			data->lasteat[id - 1] = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-			pthread_mutex_unlock(&data->lasteatmutex);
-			display(data->t_zero, id, &data->display, "is eating");
-			pthread_mutex_lock(&data->meals);
-			data->mealcount[id - 1]++;
-			pthread_mutex_unlock(&data->meals);
-			usleep(data->timetoeat * 1000);
-			pthread_mutex_lock(&data->forkmutex[(((id - 1) / 2) * 2 + 1) % data->philnbr]);
-			pthread_mutex_lock(&data->forkmutex[((id / 2) * 2) % data->philnbr]);
-			data->forktab[(((id - 1) / 2) * 2 + 1) % data->philnbr] = 1;
-			data->forktab[((id / 2) * 2) % data->philnbr] = 1;
-			pthread_mutex_unlock(&data->forkmutex[(((id - 1) / 2) * 2 + 1) % data->philnbr]);
-			eat_sleep_think = 2;
-		}
-		pthread_mutex_unlock(&data->forkmutex[((id / 2) * 2) % data->philnbr]);
-		pthread_mutex_lock(&data->forkmutex[(((id - 1) / 2) * 2 + 1) % data->philnbr] );
-		if (data->forktab[(((id - 1) / 2) * 2 + 1) % data->philnbr] && eat_sleep_think == 0)
-		{			
-			data->forktab[(((id - 1) / 2) * 2 + 1) % data->philnbr] = 0;
-			display(data->t_zero, id, &data->display, "has taken a fork");
+			takes_put_fork(data, id, 1, 0);	
 			eat_sleep_think = 1;
-		}
-		pthread_mutex_unlock(&data->forkmutex[(((id - 1) / 2) * 2 + 1) % data->philnbr]);	
-		pthread_mutex_lock(&data->endmutex);
+		}		
 	}
-	pthread_mutex_unlock(&data->endmutex);
 	return (0);
 }
 
-int	check_end(void *arg)
+int	ender(void *arg)
 {
 	t_data *data = (t_data *)arg;
 	struct timeval tv;
@@ -218,7 +261,7 @@ int	main(int ac, char **av)
 		usleep(10);
 		i++;
 	}
-	if (check_end(&data) == 0)
+	if (ender(&data) == 0)
 	{
 		i = -1;
 		while (++i < data.philnbr)
